477d71927489b82e9057c340dc7f1e448578f93a
pragatiprabhu24 <prabhupragati5@gmail.com>
2024-03-15 12:55:59 +0530

    added logo in login page and active login button changes

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,6 +1,34 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../assets/images/logo.png";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+    checkFormValidity();
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+    checkFormValidity();
+  };
+
+  const checkFormValidity = () => {
+    if (email.trim() !== "" && password.trim() !== "") {
+      setIsFormValid(true);
+    } else {
+      setIsFormValid(false);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="relative p-5 min-h-screen">
       <img
@@ -13,20 +41,21 @@ const LoginPage = () => {
         src="https://i.insider.com/5d3228152516e93f46389b5f?width=700"
         alt=""
       />
-
       <section className="relative pt-2 px-4 lg:px-6 pb-20 md:pb-20 overflow-hidden">
         <div className="relative max-w-10xl pt-5 sm:pt-28 mx-auto">
           <div className="relative container px-4 mx-auto">
             <div className="max-w-lg md:max-w-xl py-10 px-6 xs:px-12 lg:px-16 mx-auto bg-white rounded-4xl shadow-lg">
-              <h1 className="text-center text-3xl font-semibold mb-4">
-                Balarch
-              </h1>
+              <div className="flex flex-col items-center justify-center">
+                <NavLink to="/">
+                  <img src={logo} className="w-40 mb-5" alt="Logo" />
+                </NavLink>
 
-              <p className="text-lg text-gray-500 mb-10">
-                Greetings on your return! We kindly request you to enter your
-                details.
-              </p>
-              <form>
+                <p className="text-lg text-gray-500 mb-10">
+                  Greetings on your return! We kindly request you to enter your
+                  details.
+                </p>
+              </div>
+              <form onSubmit={handleSubmit}>
                 <div className="mb-6">
                   <label
                     className="block mb-1.5 text-sm text-gray-900 font-semibold"
@@ -37,7 +66,9 @@ const LoginPage = () => {
                   <input
                     className="w-full py-3 px-4 text-sm text-gray-900 placeholder-gray-400 border border-gray-200 focus:border-purple-500 focus:outline-purple rounded-lg"
                     type="email"
-                    placeholder="pat@saturn.dev"
+                    placeholder="john@gmail.com"
+                    value={email}
+                    onChange={handleEmailChange}
                   />
                 </div>
                 <div className="mb-6">
@@ -51,7 +82,9 @@ const LoginPage = () => {
                     <input
                       className="w-full py-3 px-4 text-sm text-gray-900 placeholder-gray-400 border border-gray-200 focus:border-purple-500 focus:outline-purple rounded-lg"
                       type="password"
-                      placeholder="min 12 cars"
+                      placeholder="********"
+                      value={password}
+                      onChange={handlePasswordChange}
                     />
                     <button className="absolute top-1/2 right-0 mr-3 transform -translate-y-1/2 inline-block hover:scale-110 transition duration-100">
                       <img
@@ -68,11 +101,15 @@ const LoginPage = () => {
                   </label>
                 </div>
                 <button
-                  className="relative group block w-full mb-6 py-3 px-5 text-center text-sm font-semibold text-orange-50 bg-sky-500 rounded-full overflow-hidden"
+                  className={`relative group block w-full mb-6 py-3 px-5 text-center text-sm font-semibold text-orange-50 bg-gray-800 ${
+                    isFormValid
+                      ? "hover:bg-blue-gray-900"
+                      : "cursor-not-allowed opacity-50"
+                  } rounded-full overflow-hidden`}
                   type="submit"
+                  disabled={!isFormValid}
                 >
-                  <div className="absolute top-0 right-full w-full h-full bg-gray-900 transform group-hover:translate-x-full group-hover:scale-102 transition duration-500"></div>
-                  <span className="relative">Sign In</span>
+                  <span className="relative">Log In</span>
                 </button>
                 <div className="text-center">
                   <span className="text-xs font-semibold text-gray-900">
